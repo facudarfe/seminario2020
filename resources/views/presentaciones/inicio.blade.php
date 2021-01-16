08036e9ee4da20e92b79e68cdaea8ff4efd93b2b
Facundo Darfe <facudarfe@exa.unsa.edu.ar>
2021-01-16 20:12:26 -0300

    Creacion de layout para las paginas con tablas

--- resources/views/presentaciones/inicio.blade.php
+++ resources/views/presentaciones/inicio.blade.php
@@ -1,9 +1,27 @@
-@extends('layouts.aplicacion')
+@extends('layouts.tabla')
 
-@section('titulo', 'Presentaciones')
-    
+@section('otros-estilos')
+    <link href="{{asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+@endsection
+
+@section('titulo', 'Presentaciones')  
 @section('titulo-contenido', 'Presentaciones')
 
-@section('contenido')
+@section('titulo-tabla', 'Presentaciones')
     
+@section('contenido-tabla')
+    <thead>
+
+    </thead>
+    <tbody>
+
+    </tbody>
+@endsection
+
+@section('otros-scripts')
+    <!-- Datatables-->
+    <script src="{{asset('sbadmin/vendor/datatables/jquery.dataTables.min.js')}}"></script>
+    <script src="{{asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
+    <!-- Page level custom scripts -->
+    <script src="{{asset('js/dataTables.js')}}"></script>
 @endsection
