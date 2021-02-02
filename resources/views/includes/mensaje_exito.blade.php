ce9eaad7aff7a4f377cdce3ff1fa6592b1a7636b
Facundo Darfe <facudarfe@exa.unsa.edu.ar>
2021-02-01 23:20:24 -0300

    style: Agregar icono de tilde en mensajes exitosos

--- resources/views/includes/mensaje_exito.blade.php
+++ resources/views/includes/mensaje_exito.blade.php
@@ -2,6 +2,7 @@
     <div class="col-12">
         @if (session('exito'))
         <div class="alert alert-success alert-dismissable fade show">
+            <i class="fas fa-check"></i>
             {{session('exito')}}
             <a href="#" class="close" data-dismiss="alert">&times;</a>
         </div>
