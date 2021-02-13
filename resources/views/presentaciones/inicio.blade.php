9953272f1f7044d3d5377fa81d61d8e784737cfa
Facundo Darfe <facudarfe@exa.unsa.edu.ar>
2021-02-13 19:09:42 -0300

    add: Exportar tabla de presentaciones a Excel y PDF

--- resources/views/presentaciones/inicio.blade.php
+++ resources/views/presentaciones/inicio.blade.php
@@ -2,6 +2,16 @@
 
 @section('otros-estilos')
     <link href="{{asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/buttons/1.6.5/css/buttons.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/searchbuilder/1.0.1/css/searchBuilder.bootstrap4.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/datetime/1.0.2/css/dataTables.dateTime.min.css">
+    <style>
+        /*Cambiar la posicion del input de busqueda del DataTable a la izquierda reescribiendo las clases*/
+        div.dataTables_wrapper div.dataTables_filter{
+            text-align: left;
+        }
+    </style>
 @endsection
 
 @section('titulo', 'Presentaciones')  
@@ -26,16 +36,18 @@
 @section('contenido-tabla')
 <table class="table" id="dataTable" width="100%" cellspacing="0" data-role="{{auth()->user()->getRoleNames()->first()}}">
     <thead>
-        <th></th>
-        <th>Fecha</th>
-        <th>Titulo</th>
-        @unlessrole('Estudiante')
-        <th>Alumno</th>
-        @endrole
-        <th>Director</th>
-        <th>Modalidad</th>
-        <th>Estado</th>
-        <th>Acciones</th>
+        <tr>
+            <th></th>
+            <th>Fecha</th>
+            <th>Titulo</th>
+            @unlessrole('Estudiante')
+            <th>Alumno</th>
+            @endrole
+            <th>Director</th>
+            <th>Modalidad</th>
+            <th>Estado</th>
+            <th>Acciones</th>
+        </tr>
     </thead>
     <tbody>
         @foreach ($presentaciones as $presentacion)
@@ -116,35 +128,22 @@
     <!-- Datatables-->
     <script src="{{asset('sbadmin/vendor/datatables/jquery.dataTables.min.js')}}"></script>
     <script src="{{asset('sbadmin/vendor/datatables/dataTables.bootstrap4.min.js')}}"></script>
-    <script>
-        $(document).ready(function(){
-            var rol = $('#dataTable').data('role');
-            
-            if(rol != 'Estudiante'){
-                $('#dataTable').DataTable({
-                    "columnDefs": [{
-                        "targets": 0,
-                        "orderable": false
-                    }],
-                    "language": {
-                        "url": "//cdn.datatables.net/plug-ins/1.10.15/i18n/Spanish.json"
-                    }
-                });
-            }
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.bootstrap4.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+    <script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
+    <script src="https://cdn.datatables.net/searchbuilder/1.0.1/js/dataTables.searchBuilder.min.js"></script>
+    <script src="https://cdn.datatables.net/searchbuilder/1.0.1/js/searchBuilder.bootstrap4.min.js"></script>
+    <script src="https://cdn.datatables.net/datetime/1.0.2/js/dataTables.dateTime.min.js"></script>
 
-            //Subida del informe
-            $('#botonInforme').click(function(){
-                var id = $(this).data('presentacion');
-
-                $('#modalInforme').modal('show');
-                $('#modalInforme').ready(function(){
-                    $('#idPresentacion').val(id);
-                });
-            });
-        });
-    </script>
     @include('includes.scripts_validaciones')
+    
     <!--Script adicional para jQuery validation para validar las extensiones de los archivos-->
     <script src="https://ajax.aspnetcdn.com/ajax/jquery.validate/1.15.0/additional-methods.js"></script>
+
+    <!--Script propio para este pagina-->
     <script src="{{asset('js/presentaciones/inicio.js')}}"></script>
 @endsection
--- resources/views/presentaciones/ver.blade.php
+++ resources/views/presentaciones/ver.blade.php
@@ -93,7 +93,7 @@
                                     <div class="row">
                                         <div class="col-12">
                                             <h5>Tecnologias: </h5>
-                                            <p>{!! nl2br($version->tecnologias) !!}}</p>
+                                            <p>{!! nl2br($version->tecnologias) !!}</p>
                                         </div>
                                     </div>
                                     <div class="row">
