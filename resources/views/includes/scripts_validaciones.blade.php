fd2842e73486c57a498d75b418d48d4c930b048a
Facundo Darfe <facudarfe@exa.unsa.edu.ar>
2021-04-11 21:30:28 -0300

    fix: Solucionar validacion de extension del lado del cliente

--- resources/views/includes/scripts_validaciones.blade.php
+++ resources/views/includes/scripts_validaciones.blade.php
@@ -1,3 +1,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-validate/1.19.0/jquery.validate.min.js"></script>
+
+<!--Script adicional para jQuery validation para validar las extensiones de los archivos-->
+<script src="{{asset('js/additional-methods.min.js')}}"></script>
+
 <script src="{{asset('js/validaciones/idiomas/es_AR.js')}}"></script>
 <script src="{{asset('js/validaciones/initValidation.js')}}"></script>
